7c5b38b0cd011492b5927a47a95e2241c6aa3015
bal12 <balemah@outlook.com>
2022-06-13 11:50:42 +0545

    new style guide

--- dev/scripts/modules/slider.js
+++ dev/scripts/modules/slider.js
@@ -315,15 +315,11 @@
 // new slider();
 
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper('.slider__slides-hold', {
-//     // configure Swiper to use modules
-//     modules: [Navigation, Pagination],
-//     ...
-//   });
 
-  console.log('hello world');
+const swiper = new Swiper('.slider__slides-hold', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination]
+  });
+
