b2c0b0dc6987ae50b3aff255f77430b91bd3c92d
bal12 <balemah@outlook.com>
2022-06-13 14:02:05 +0545

    pr fixes

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -25,51 +25,6 @@ class global {
     slider () {
         swiper();
     }
-    
-    stickyHeader() {
-        var elemBody = $('body');
-        var pageHeader = $('.theme-header');
-        if ($(elemBody).hasClass('home')) {
-            $(pageHeader).addClass('theme-header--dark');
-        }
-        
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('.theme-header').addClass('sticky');
-            } else {
-                $('.theme-header').removeClass('sticky');
-            }
-        });
-    }
-    
-    megamenuPopup() {
-        var toggleBar = $('.navbar-toggle');
-        var popupMenu = $('.megamenu-popup');
-        var closePopup = $('.close-menu');
-        $(toggleBar).click(function () {
-            $(popupMenu).addClass('active');
-        });
-        $(closePopup).click(function () {
-            $(popupMenu).removeClass('active');
-        });
-    }
-    
-    toggleContent() {
-        var toggleContent = $('.toggle-text');
-        var viewBtn = $('.toggleRead');
-        $(viewBtn).click(function () {
-            $(this).toggleClass('active');
-            $(toggleContent).toggleClass('active');
-        });
-    }
-    
-    readMore() {
-        $('.readmore').click(function () {
-            $('.collapse-text').addClass('expand');
-            $(this).hide();
-        });
-    }
-    
 }
 
 export default global;
