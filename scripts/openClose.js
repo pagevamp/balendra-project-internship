c54c14acef871f65154f0973f8d93dd9c91a1841
Balendra-Maharjan <balemah@outlook.com>
2022-06-21 09:33:55 +0000

    Deploying to gh-pages from @ pagevamp/balendra-project-internship@1525836eab73ad0272ed15167f952d5c4c781d2c 🚀

--- scripts/openClose.js
+++ scripts/openClose.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";window.onresize=function(){if(window.innerWidth<=1200){const e=document.querySelectorAll(".toggle-info");let t=document.querySelectorAll(".toggle-list");console.log(t),e.forEach((e=>{e.addEventListener("click",(e=>{e.target.nextSibling.classList.toggle("show-element")}))}))}}})();
+(()=>{"use strict";window.addEventListener("resize",(function(){window.innerWidth<=1200&&document.querySelectorAll(".opener").forEach((e=>{e.addEventListener("click",(e=>{e.target.nextSibling.classList.toggle("show-element")}))}))}))})();
