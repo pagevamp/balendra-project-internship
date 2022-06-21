02b29f63002156a5ccbcf31cb50f72a0d5f5618f
bal12 <balemah@outlook.com>
2022-06-21 13:17:33 +0545

    footer js fixed

--- dev/scripts/modules/openClose.js
+++ dev/scripts/modules/openClose.js
@@ -1,32 +1,24 @@
-import { node } from "webpack";
-
 export function openClose() {
     // console.log(window.innerWidth);
     if(window.innerWidth <= 1200){
-        console.log('reached here');
-        var toogleInfos = document.getElementsByClassName('toggle-info');
-        console.log(toogleInfos);
+        // console.log('reached here');
+        const toogleInfos = document.querySelectorAll('.toggle-info');
+        let toggleContent = document.querySelectorAll('.toggle-list');
         
-        for(let i=0; i<toogleInfos.length; i++){
-            toogleInfos[i].addEventListener("click", function() {
-                toggleDiv(this);
-                console.log('clicked');
-            });
-        }
-    }    
-}
-
-function toggleDiv(node) {
-    // console.log(node);
-    let toggleContent = node.getElementsByClassName('toggle-list')[0];
-    console.log(toggleContent);
-    
-    if(toggleContent.style.display === 'block'){
-        toggleContent.style.display = 'none';
+        console.log(toggleContent);
+        toogleInfos.forEach(item => {
+            item.addEventListener('click', event => {
+                //handle click
+                // console.log(event.target.nextSibling);
+                event.target.nextSibling.classList.toggle('show-element');
+                
+                
+            })
+            
+            
+        })
     }
-    else{
-        toggleContent.style.display = 'block';
-    }    
 }
 
+window.onresize = openClose;
 
