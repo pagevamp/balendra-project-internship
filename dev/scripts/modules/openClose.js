cb766d119b9da71a1a270751b715a80f192e4bc0
bal12 <balemah@outlook.com>
2022-06-21 15:15:22 +0545

    footer js fixed

--- dev/scripts/modules/openClose.js
+++ dev/scripts/modules/openClose.js
@@ -1,32 +1,18 @@
-import { node } from "webpack";
-
 export function openClose() {
     // console.log(window.innerWidth);
     if(window.innerWidth <= 1200){
-        console.log('reached here');
-        var toogleInfos = document.getElementsByClassName('toggle-info');
-        console.log(toogleInfos);
-        
-        for(let i=0; i<toogleInfos.length; i++){
-            toogleInfos[i].addEventListener("click", function() {
-                toggleDiv(this);
-                console.log('clicked');
-            });
-        }
-    }    
-}
-
-function toggleDiv(node) {
-    // console.log(node);
-    let toggleContent = node.getElementsByClassName('toggle-list')[0];
-    console.log(toggleContent);
-    
-    if(toggleContent.style.display === 'block'){
-        toggleContent.style.display = 'none';
+        // console.log('reached here');
+        const opener = document.querySelectorAll('.opener');
+       
+        // console.log(toggleContent);
+        opener.forEach(item => {
+            item.addEventListener('click', event => {
+                //handle click
+                // console.log(item.nextSibling);
+                event.target.nextSibling.classList.toggle('show-element');
+            })
+        })
     }
-    else{
-        toggleContent.style.display = 'block';
-    }    
 }
-
-
+// window.onresize = openClose;
+window.addEventListener('resize', openClose);
